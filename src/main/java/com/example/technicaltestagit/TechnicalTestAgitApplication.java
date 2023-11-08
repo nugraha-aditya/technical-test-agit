1391fd17acb58f1c0ed8392806124651f7a5a617
nugraha-aditya <adityapradana73@gmail.com>
2023-11-09 00:19:15 +0700

    [adit] initialize project

---
+++ src/main/java/com/example/technicaltestagit/TechnicalTestAgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.technicaltestagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestAgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalTestAgitApplication.class, args);
+    }
+
+}
